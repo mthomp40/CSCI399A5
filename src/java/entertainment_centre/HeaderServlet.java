085e9c6b082adae1a8892cb4e7f97076abba1a7e
mthomp40 <mthommo79@gmail.com>
2015-05-17 06:53:58 +1000

    Initial

---
+++ src/java/entertainment_centre/HeaderServlet.java
@@ -0,0 +1,40 @@
+package entertainment_centre;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HeaderServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<div id='headdiv'>");
+            out.println("<img src='/CSCI399A5/public/siteimages/logo.png'>");
+            out.println("<div id='ident'><p>Entertainment Centre</p></div>");
+            out.println("</div>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
